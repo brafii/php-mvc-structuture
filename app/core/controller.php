e3573cd6a95e4b3985f5ede7ffccd6b64a700b34
Fiifi Ofori <fiifi56@gmail.com>
2022-04-14 09:15:21 +0000

    MVC Structure

---
+++ app/core/controller.php
@@ -0,0 +1,17 @@
+<?php
+
+class Controller
+{
+    public function view($path, $data = [])
+    {
+
+        if(file_exists('../app/views/' . $path . '.php'))
+        {
+            include_once '../app/views/' . $path . '.php';
+        }
+
+    }
+}
+
+
+?>
---
+++ app/init.php
@@ -0,0 +1,9 @@
+<?php
+
+    //Require libraries from the folder libraries
+    require_once '../app/core/controller.php';
+    require_once '../app/core/database.php';
+    require_once '../app/core/functions.php';
+    require_once '../app/core/app.php'; 
+
+?>
